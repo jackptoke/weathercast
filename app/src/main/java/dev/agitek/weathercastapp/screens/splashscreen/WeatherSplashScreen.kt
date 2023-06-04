4d2665572bced6e3669208fc9716986755f5dd07
Jack Toke <54049012+jackptoke@users.noreply.github.com>
2023-06-04 23:38:47 +1000

    Navigation setup completed

--- app/src/main/java/dev/agitek/weathercastapp/screens/splashscreen/WeatherSplashScreen.kt
+++ app/src/main/java/dev/agitek/weathercastapp/screens/splashscreen/WeatherSplashScreen.kt
@@ -1,5 +1,8 @@
 package dev.agitek.weathercastapp.screens.splashscreen
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -8,27 +11,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Star
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import dev.agitek.weathercastapp.R
+import dev.agitek.weathercastapp.navigation.WeatherScreens
+import kotlinx.coroutines.delay
 
 @Composable
 fun WeatherSplashScreen(navController: NavController) {
+    val scale = remember{ Animatable(0f) }
+    LaunchedEffect(key1 = true, block = {
+        scale.animateTo(targetValue = 0.9f, animationSpec = tween(
+            durationMillis = 800,
+            easing = { OvershootInterpolator(8f).getInterpolation(it) }
+        ))
+        delay(2000L)
+        navController.navigate(WeatherScreens.MainScreen.name)
+    })
     Surface(modifier = Modifier
         .padding(15.dp)
-        .size(330.dp)
-        .background(Color.Transparent),
+        .size(230.dp)
+        .background(Color.Transparent)
+        .scale(scale.value),
         shape = CircleShape,
         border = BorderStroke(width = 3.dp, color = Color.LightGray)
     ) {
@@ -36,8 +52,9 @@ fun WeatherSplashScreen(navController: NavController) {
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center) {
             Image(painter = painterResource(id = R.drawable.cloud),
-                contentDescription = "Cloud icon", modifier = Modifier.size(55.dp))
-            Text("WeatherCast", style = MaterialTheme.typography.titleMedium)
+                contentDescription = "Cloud icon", contentScale = ContentScale.Fit, modifier = Modifier.size(55.dp))
+            Text("WeatherCast", style = MaterialTheme.typography.titleMedium,
+                color = Color.LightGray)
         }
     }
 
